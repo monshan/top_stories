9d15a47ce1225e49de8806f1e69105efdefc502b
Marika <marikashanahan@gmail.com>
2021-06-09 01:23:24 -0600

    Add hover to show title transition

--- src/ArticleCard/ArticleCard.js
+++ src/ArticleCard/ArticleCard.js
@@ -2,8 +2,10 @@ import './ArticleCard.css';
 
 export const ArticleCard = ({ title, media }) => {
   return (
-    <div className="ArticleCard">
-      {/* <p>{ title }</p> */}
+    <div className="article-card">
+      <div className="article-card__title-hover">
+        <p>{ title }</p>
+      </div>
       <img src={media} />
     </div>
   )
