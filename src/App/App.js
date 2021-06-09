515c9702976054bc5ef7a7eea5f7df7722f9f75c
Marika <marikashanahan@gmail.com>
2021-06-08 23:42:25 -0600

    Write basic setup of  state with hooks to determine topic

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import { getArticles } from '../calls'; 
+
 import './App.css';
 
+
 const App = () => {
+  const [topic, setTopic] = useState('');
+  const [articlesArray, setArticlesArray] = useState([]);
+
+  const helper = () => {
+    const userTopic = document.getElementById('userTopic');
+    return setTopic(userTopic.value);
+  }
+
+  const populate = () => {
+    return setArticlesArray(getArticles(topic));
+  }
+
   return (
     <div className="App">
       <p>Eventual Container</p>
+      <input type="text" id="userTopic" onChange={ () => helper() }/>
+      <button
+        onClick={() => populate()}
+      >Submit</button>
+      <div>
+        <p>{ articlesArray[0] }</p>
+      </div>
     </div>
   )
 }
---
+++ src/calls.js
@@ -0,0 +1,14 @@
+export const getArticles = genre => {
+  fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=1A5tcEY81u6UG91Av0Av7b81eAm4WHFY`)
+    .then(res => {
+      console.log(res);
+      if (res.status !== "OK") throw new Error();
+    })
+    .then(goodRes => {
+      return goodRes.results;
+    })
+    .catch(badRes => {
+      console.log(badRes);
+      return badRes;
+    })
+}
