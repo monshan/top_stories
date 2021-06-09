1443e1d157eb182d3c186e52c98141eaa34293dc
Marika <marikashanahan@gmail.com>
2021-06-09 00:31:56 -0600

    Move then statementss to App component for successful GET request

--- src/App/App.js
+++ src/App/App.js
@@ -1,35 +1,46 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getArticles } from '../calls'; 
-
 import './App.css';
-
 
 const App = () => {
   const [section, setSection] = useState('');
-  const [articlesArray, setArticlesArray] = useState(null);
+  const [articlesArray, setArticlesArray] = useState([]);
 
   const allTopics = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'];
+
+  useEffect(() => {
+    cleanGETbySection();
+  }, [])
+
+  const cleanGETbySection = () => {
+    getArticles(section)
+      .then(res => {
+        // console.log(res);
+        if (!res.ok) throw new Error();
+        return res.json();
+      })
+      .then(goodRes => {
+        // console.log(goodRes.results);
+        setArticlesArray(goodRes.results);
+      })
+      .catch((badRes) => {
+        // console.log(badRes);
+        return badRes;
+      })
+  }
 
   const helper = () => {
     const userTopic = document.getElementById('userTopic');
     return setSection(userTopic.value);
   }
 
-  const populate = () => {
-    const results = getArticles(section);
-    return setArticlesArray(results);
-  }
-
   return (
     <div className="App">
       <p>Eventual Container</p>
-      <input type="text" id="userTopic" onChange={ () => helper() }/>
+      <input type="text" id="userTopic" />
       <button
-        onClick={() => populate()}
+        onClick={() => helper()}
       >Submit</button>
-      <div>
-        <p>{ articlesArray }</p>
-      </div>
     </div>
   )
 }
--- src/calls.js
+++ src/calls.js
@@ -1,16 +1,3 @@
 export const getArticles = genre => {
-  fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=1A5tcEY81u6UG91Av0Av7b81eAm4WHFY`)
-    .then((res) => {
-      // console.log(res);
-      if (!res.ok) throw new Error();
-      return res.json();
-    })
-    .then((goodRes) => {
-      // console.log(goodRes);
-      return goodRes.results;
-    })
-    .catch((badRes) => {
-      // console.log(badRes);
-      return badRes;
-    })
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=1A5tcEY81u6UG91Av0Av7b81eAm4WHFY`);
 }
