ff5899ab5a3a97aa40515d1895c21ca158c8649a
Marika <marikashanahan@gmail.com>
2021-06-11 01:06:14 -0600

    Define active styles to NavLinks

--- src/App/App.js
+++ src/App/App.js
@@ -5,9 +5,18 @@ import './App.css';
 const App = () => {
   const allTopics = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'];
 
+  const NavLinkActiveStyle = () => {
+    return {
+      fontFamily: "'Playfair Display', serif",
+      fonstSize: "1.5rem",
+      fontStyle: "italic",
+      borderBottom: "2px solid cyan"
+    }
+  }
+
   const generateNavLinks = () => {
     return allTopics.map(topic => {
-      return <NavLink to={ `/${topic}` } className="NavLink">{ topic }.</NavLink>
+      return <NavLink to={ `/${topic}` } className="NavLink" activeStyle={ NavLinkActiveStyle() }>{ topic }.</NavLink>
     })
   }
 
