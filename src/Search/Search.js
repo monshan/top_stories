f0740b2d6b097c788edcf33c3acafbf2634252d1
Marika <marikashanahan@gmail.com>
2021-06-10 22:44:33 -0600

    Add margin and text styling to Search label and input

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
+import './Search.css';
+
 export const Search = ({ section, setFormValue }) => {
 
   const targetForm = document.getElementById('searchForm');
 
   return (
-    <div>
-      <label for="searchForm">Search {section} for </label>
+    <div className="Search">
+      <label for="searchForm">Search {section} for ... </label>
       <input type="text" id="searchForm" onChange={() => setFormValue(targetForm.value)}/>
     </div>
   )
